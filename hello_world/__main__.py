ce13498a03676552fa9d334dd9d156d6b46cd532
Joshua Nolan <64180446+doctorLightbulb@users.noreply.github.com>
2025-02-27 16:29:19 -0800

    Added time.sleep() to slow when the terminal window closes.

--- hello_world/__main__.py
+++ hello_world/__main__.py
@@ -1,9 +1,12 @@
 """A simple `Hello, World!` example intended as a cross-compilation text."""
 
+import time
+
 
 def main():
     """Entry point for `Hello, World!`"""
     print("Hello World!")
+    time.sleep(5)
 
 
 if __name__ == "__main__":
